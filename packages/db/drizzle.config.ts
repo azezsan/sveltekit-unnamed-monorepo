25e89107ffe94237537241467a8b6e70b08c2900
azez <t.s.connect.m@gmail.com>
2024-05-12 18:30:34 +0300

    fix drizzle

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -8,6 +8,6 @@ if (!process.env.DB_URL) throw new Error("DB_URL not set")
 
 export default {
   schema: "./src/lib/schema.ts",
-  driver: "mysql2",
-  dbCredentials: { uri: process.env.DB_URL }
+  dialect: 'mysql',
+  dbCredentials: { url: process.env.DB_URL }
 } satisfies Config;
